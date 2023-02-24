5eab8e91bbb4dabbff0ece6b133824a623f94618
Oleh Kliapko <goldclub1977@gmail.com>
2023-02-24 20:56:06 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,16 @@
 import { Header } from 'components';
 import { CountrySearch, Home, Country } from 'pages';
+import { Navigate, Route, Routes } from 'react-router-dom';
 
 export const App = () => {
-  return <h2>App</h2>;
+  return (
+    <Routes>
+      <Route path="/" element={<Header />}>
+        <Route index element={<Home />} />
+        <Route path="country" element={<Country />} />
+        <Route path="country/:countryId" element={<CountrySearch />} />
+      </Route>
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
 };
