5cbb05557803baf49db8ff60b47a000990d6a5c4
Oleh Kliapko <goldclub1977@gmail.com>
2023-02-24 22:15:06 +0200

    add searching

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,14 +1,33 @@
 import { FiSearch } from 'react-icons/fi';
+import { useState } from 'react';
 import { BtnSearch, Select, SearchFormStyled } from './SearchForm.styled';
+import PropTypes from 'prop-types';
 
-export const SearchForm = () => {
+export const SearchForm = ({ onSubmit }) => {
+  const [query, setQuery] = useState('');
+
+  const handleChange = evt => {
+    setQuery(evt.target.value);
+  };
+
+  const handleSubmit = evt => {
+    evt.preventDefault();
+    onSubmit(query);
+  };
+
   return (
-    <SearchFormStyled>
+    <SearchFormStyled onSubmit={handleSubmit}>
       <BtnSearch type="submit">
         <FiSearch size="16px" />
       </BtnSearch>
-      <Select aria-label="select" name="region" required>
-        <option selected disabled defaultValue="">
+      <Select
+        aria-label="select"
+        name="region"
+        required
+        onChange={handleChange}
+        defaultValue="DEFAULT"
+      >
+        <option value="DEFAULT" disabled defaultValue="">
           Select a region and press enter
         </option>
         <option value="africa">Africa</option>
@@ -19,4 +38,8 @@ export const SearchForm = () => {
       </Select>
     </SearchFormStyled>
   );
+};
+
+SearchForm.propTypes = {
+  onSubmit: PropTypes.func,
 };
--- src/pages/CountrySearch.jsx
+++ src/pages/CountrySearch.jsx
@@ -6,12 +6,38 @@ import {
   Loader,
   CountryList,
 } from 'components';
+import { useSearchParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { fetchByRegion } from 'service/country-service';
 
 export const CountrySearch = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [countries, setCountries] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const region = searchParams.get('region');
+
+    if (!region) return;
+
+    setIsLoading(true);
+
+    fetchByRegion(region)
+      .then(setCountries)
+      .catch(error => error.message)
+      .finally(() => setIsLoading(false));
+  }, [searchParams]);
+
+  const onSubmit = region => {
+    setSearchParams({ region });
+  };
+
   return (
     <Section>
       <Container>
-        <h2>CountrySearch</h2>
+        <SearchForm onSubmit={onSubmit} />
+        {isLoading && <Loader />}
+        {!isLoading && <CountryList countries={countries} />}
       </Container>
     </Section>
   );
