495cab0a64b868289006306e1332569878ebd1ac
InnaLyksa <in.lyksa@gmail.com>
2023-02-24 21:48:01 +0200

    Task-1

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,10 +1,20 @@
 import { Section, Container, CountryInfo, Loader } from 'components';
+import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { fetchCountry } from 'service/country-service';
 
 export const Country = () => {
+  const { countryId } = useParams();
+  const [country, setCountry] = useState({});
+  // console.log(countryId);
+  useEffect(() => {
+    fetchCountry(countryId).then(setCountry);
+  }, [countryId]);
+
   return (
     <Section>
       <Container>
-        <h2>Country</h2>
+        <CountryInfo country={country} />
       </Container>
     </Section>
   );
